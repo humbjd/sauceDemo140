9d208170bb4ae4cbeebe7cf92a1cd67078c418d1
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2024-03-13 22:13:16 -0300

    implementação dos asserts

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -1,6 +1,7 @@
 # 1 - Bibliotecas
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
 # 2 - Classe (Opcional)
@@ -16,7 +17,13 @@ class Test_Products():
     def teardown_method(self, method):                    # método de finalização dos teste
         self.driver.quit()                                # encerra / destrói o objeto do Selenium Webdriver da memória
 
-    def test_select_products(self):               # método de teste
-        self.driver.get(self.url)                         # abre o navegador
-        self.driver.find_element(By.ID,'user-name').send_keys('standard_user')      # escreve no campo user-name
-        self.driver.find_element(By.NAME,'password').send_keys('secret_sauce')      # escreve no campo password
+    def test_select_products(self):                         # método de teste
+        self.driver.get(self.url)                           # abre o navegador
+        self.driver.find_element(By.ID,'user-name').send_keys('standard_user')              # escreve no campo user-name
+        self.driver.find_element(By.NAME,'password').send_keys('secret_sauce')              # escreve no campo password
+        self.driver.find_element(By.CSS_SELECTOR,'input.submit-button.btn_action').click()  # clique no botão login
+
+        # transição de página
+        assert self.driver.find_element(By.CSS_SELECTOR, 'span.title').text == 'Products'   # checa o elemento está escrito Products
+        assert self.driver.find_element(By.ID, 'item_4_title_link').text == 'Sauce Labs Backpack' # confirma se é a mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, '.inventory_item:nth-child(1) .inventory_item_price').text == '$29.99' # confere preço
