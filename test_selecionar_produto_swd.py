a5722abcbbf8fa4c24a6fae72ef60ee694fd91de
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2024-03-20 15:05:50 -0300

    implementação de codigo GF e SD

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -13,6 +13,7 @@ class Test_Products():
     def setup_method(self, method):                       # método de inicialização dos testes 
         self.driver = webdriver.Chrome()                  # instanciar o projeto do Selenium Webdriver como Chrome
         self.driver.implicitly_wait(10)                   # irá esperar qualquer elemento por até 10 segundos (recomendado)
+        self.driver.maximize_window()
 
     def teardown_method(self, method):                    # método de finalização dos teste
         self.driver.quit()                                # encerra / destrói o objeto do Selenium Webdriver da memória
@@ -27,7 +28,13 @@ class Test_Products():
         assert self.driver.find_element(By.CSS_SELECTOR, 'span.title').text == 'Products'   # checa o elemento está escrito Products
         assert self.driver.find_element(By.ID, 'item_4_title_link').text == 'Sauce Labs Backpack' # confirma se é a mochila
         assert self.driver.find_element(By.CSS_SELECTOR, '.inventory_item:nth-child(1) .inventory_item_price').text == '$29.99' # confere preço
-        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
-        assert self.driver.find_element(By.CSS_SELECTOR, 'span.class').text == '1'
-        self.driver.find_element(By.CSS_SELECTOR, "shopping_cart_badge").click()
+        self.driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack').click()
+        self.driver.find_element(By.CSS_SELECTOR, "#shopping_cart_container").click()
+        assert self.driver.find_element(By.CSS_SELECTOR, 'span.title').text == 'Your Cart'
+        assert self.driver.find_element(By.CSS_SELECTOR, '#item_4_title_link').text == 'Sauce Labs Backpack'
+        assert self.driver.find_element(By.CSS_SELECTOR, '.inventory_item_price').text == '$29.99'
+        self.driver.find_element(By.CSS_SELECTOR, '#remove-sauce-labs-backpack').click()
+        self.driver.find_element(By.CSS_SELECTOR, '#react-burger-menu-btn'). click()
+        self.driver.find_element(By.CSS_SELECTOR, '#logout_sidebar_link').click()
+
         
